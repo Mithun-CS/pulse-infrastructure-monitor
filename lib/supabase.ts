d4f193b13500438da8c339bae98d32ac63f69d56
Mithun <pmithunkrishna20@gmail.com>
2026-04-02 15:35:23 +0530

    fix: updated env variables to next_public for vercel

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,13 +1,13 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 if (!supabaseUrl) {
-  throw new Error("Missing SUPABASE_URL in environment (.env.local)")
+  throw new Error("Missing NEXT_PUBLIC_SUPABASE_URL in environment (.env.local)")
 }
 if (!supabaseAnonKey) {
-  throw new Error("Missing SUPABASE_ANON_KEY in environment (.env.local)")
+  throw new Error("Missing NEXT_PUBLIC_SUPABASE_ANON_KEY in environment (.env.local)")
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
